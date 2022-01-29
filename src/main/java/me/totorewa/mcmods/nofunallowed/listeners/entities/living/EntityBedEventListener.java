d0b10e28f833ad7843e84820330d0d36a9c0b780
Totorewa <totorewa@outlook.com>
2022-01-29 14:31:00 +1300

    Display message to player if bed has no roof

--- src/main/java/me/totorewa/mcmods/nofunallowed/listeners/entities/living/EntityBedEventListener.java
+++ src/main/java/me/totorewa/mcmods/nofunallowed/listeners/entities/living/EntityBedEventListener.java
@@ -4,14 +4,22 @@ import me.totorewa.mcmods.fabric.api.events.entities.living.EntityBedEvent;
 import me.totorewa.mcmods.fabric.api.helpers.PositionHelper;
 import me.totorewa.mcmods.nofunallowed.NoFunAllowedConfig;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.world.entity.player.Player;
 
 public class EntityBedEventListener implements EntityBedEvent.Listener {
     @Override
     public void onUseBed(EntityBedEvent event) {
         if (NoFunAllowedConfig.requireRoofToSleep
                 && (!hasRoof(event, event.getHeadPos())
-                || !hasRoof(event, event.getFeetPos())))
+                || !hasRoof(event, event.getFeetPos()))) {
+            if (event.isPlayer()) {
+                Player player = event.getPlayer();
+                player.displayClientMessage(new TextComponent("You may not rest now; bed has no roof"), true);
+            }
             event.cancel();
+        }
     }
 
     private boolean hasRoof(EntityBedEvent event, BlockPos pos) {
